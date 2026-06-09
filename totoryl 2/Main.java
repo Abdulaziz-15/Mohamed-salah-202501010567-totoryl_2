fe8d5083fdaef2c28bcde0742f80b503bf907219
Abdulaziz-15 <azoozkelantan17@gmail.com>
2026-06-09 07:48:49 +0000

    pu

---
+++ totoryl 2/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Mohamed", 21, 3.5);
+
+        s1.displayInfo();
+    }
+}
---
+++ totoryl 2/Student.java
@@ -0,0 +1,17 @@
+class Student {
+    String name;
+    int age;
+    double gpa;
+
+    Student(String studentName, int studentAge, double studentGpa) {
+        name = studentName;
+        age = studentAge;
+        gpa = studentGpa;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("GPA: " + gpa);
+    }
+}
